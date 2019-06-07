7992f139b44e9d46ae59f3db74089e749199f7d8
KennyTheBard <octav_teculescu@yahoo.com>
2019-06-07 12:22:36 +0300

    Kind of works now

--- Perfect Cell/Automata.gd
+++ Perfect Cell/Automata.gd
@@ -1,8 +1,6 @@
 extends TileMap
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var paused = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,3 +16,42 @@ func _process(delta):
 		else:
 			tile = 0
 		set_cellv(pos, tile)
+	
+	if Input.is_action_just_pressed("pause"):
+		self.paused = !self.paused
+	
+	if not self.paused:
+		update()
+
+func apply_rule(tile_pos):
+	var neighs = []
+	for i in range(-1, 2):
+		for j in range(-1, 2):
+			if i != 0 and j != 0:
+				neighs.push_back(Vector2(tile_pos.x + i, tile_pos.y + j))
+	var live = 0
+	for pos in neighs:
+		var aux = get_cellv(pos)
+		if aux == 1:
+			live += 1
+			
+	var tile = get_cellv(tile_pos)
+	if tile == 1:
+		if live < 2 or live > 3:
+			return 0
+		else:
+			return 1
+	else:
+		if live == 3:
+			return 1
+		else:
+			return 0
+
+
+func update():
+	var cells = []
+	for tile in get_used_cells():
+		var value = apply_rule(tile)
+		cells.push_back([tile, value])
+	for cell in cells:
+		set_cellv(cell[0], cell[1])
