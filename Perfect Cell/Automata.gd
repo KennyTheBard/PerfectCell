493d76bc258f825636b6cd81b443987e651b2823
KennyTheBard <octav_teculescu@yahoo.com>
2019-06-08 12:43:51 +0300

    implemented rule 126

--- Perfect Cell/Automata.gd
+++ Perfect Cell/Automata.gd
@@ -1,5 +1,6 @@
 extends TileMap
 
+# this way would be easier to make some starting patterns
 var paused = false
 
 # Called when the node enters the scene tree for the first time.
@@ -23,19 +24,39 @@ func _process(delta):
 	if not self.paused:
 		update()
 
+func rule126(tile_pos):
+	var neighs = []
+	for i in range(-1, 2):
+		neighs.push_back(Vector2(tile_pos.x + i, tile_pos.y - 1))
+	
+	var live = 0
+	for pos in neighs:
+		var aux = get_cellv(pos)
+		if aux == 1:
+			live += 1
+	
+	var tile = get_cellv(tile_pos)
+	if tile == 0: 
+		if live == 0 or live == 3:
+			return 0
+		else:
+			return 1
+	else:
+		return tile
+
 func game_of_life(tile_pos):
 	var neighs = []
 	for i in range(-1, 2):
 		for j in range(-1, 2):
 			if i != 0 or j != 0:
 				neighs.push_back(Vector2(tile_pos.x + i, tile_pos.y + j))
+	
 	var live = 0
-	print(len(neighs))
 	for pos in neighs:
 		var aux = get_cellv(pos)
 		if aux == 1:
 			live += 1
-			
+	
 	var tile = get_cellv(tile_pos)
 	if tile == 1:
 		if live < 2 or live > 3:
@@ -52,7 +73,7 @@ func game_of_life(tile_pos):
 func update():
 	var cells = []
 	for tile in get_used_cells():
-		var value = game_of_life(tile)
+		var value = rule126(tile)
 		cells.push_back([tile, value])
 	for cell in cells:
 		set_cellv(cell[0], cell[1])
